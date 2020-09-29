6d1a544af675739fed3d6d4c355f299d79e49a1f
Matthew Bunt <darakus@dagron.net>
2020-09-29 09:16:15 -0700

    Trying out a broadcast channel

--- src/server.rs
+++ src/server.rs
@@ -1,28 +1,30 @@
 use futures::{StreamExt, SinkExt};
 use std::error::Error;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::{broadcast}
 use tokio_util::codec::{Framed, LinesCodec};
 
 pub async fn serve() -> Result<(), Box<dyn Error>> {
     let binding = ":::10113";
     let mut listener = TcpListener::bind(&binding).await?;
-    let (tx, rx) = channel(100);
+    let (client_tx, respond_rx) = broadcast::channel(100);
+    let client_rx = client_tx.subscribe();
 
     tokio::spawn(async move {
-        let _ = respond(rx).await;
+        let _ = respond(respond_rx, respond_tx).await;
     });
 
     loop {
         let (socket, _) = listener.accept().await?;
-        let tx = tx.clone();
+        let tx = client_tx.clone();
+        let rx = client_rx.clone();
         tokio::spawn(async move {
-            let _ = handle_client(socket, tx).await;
+            let _ = handle_client(socket, tx, rx).await;
         });
     }
 }
 
-async fn handle_client(socket: TcpStream, mut tx: Sender<String>) -> Result<(), Box<dyn Error>> {
+async fn handle_client(socket: TcpStream, mut tx: Sender<String>, rx: Receiver<String>) -> Result<(), Box<dyn Error>> {
     let remote_ip = socket.peer_addr()?.ip();
     println!("Incoming connection from {}", remote_ip);
 
@@ -32,12 +34,14 @@ async fn handle_client(socket: TcpStream, mut tx: Sender<String>) -> Result<(), 
             Some(Ok(q)) => q,
             _ => return Err("no query received".into()),
         };
+
+        // Echo back to client temporarily
         client.send(query.clone()).await?;
         let _ = tx.send(query).await;
     }
 }
 
-pub async fn respond(mut rx: Receiver<String>) -> Result<(), Box<dyn Error>> {
+pub async fn respond(mut rx: Receiver<String>, tx: Sender<String>) -> Result<(), Box<dyn Error>> {
     loop {
         let received = rx.recv().await;
         let received = match received {
