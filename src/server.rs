a000572e805040895544c80ef7896c8e06f74ff6
Matthew Bunt <darakus@dagron.net>
2020-09-29 12:52:55 -0700

    Channels implemented with no errors (for now)

--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,27 @@
 use futures::{StreamExt, SinkExt};
 use std::error::Error;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::{broadcast}
+use tokio::sync::{broadcast};
+use tokio::sync::broadcast::{Sender, Receiver};
 use tokio_util::codec::{Framed, LinesCodec};
 
 pub async fn serve() -> Result<(), Box<dyn Error>> {
     let binding = ":::10113";
     let mut listener = TcpListener::bind(&binding).await?;
     let (client_tx, respond_rx) = broadcast::channel(100);
-    let client_rx = client_tx.subscribe();
+    let (respond_tx, client_rx) = broadcast::channel(100);
+    
+    let r_tx = respond_tx.clone();
+    let r_rx = client_tx.subscribe();
 
     tokio::spawn(async move {
-        let _ = respond(respond_rx, respond_tx).await;
+        let _ = respond(r_rx, r_tx).await;
     });
 
     loop {
         let (socket, _) = listener.accept().await?;
         let tx = client_tx.clone();
-        let rx = client_rx.clone();
+        let rx: Receiver<String> = respond_tx.subscribe();
         tokio::spawn(async move {
             let _ = handle_client(socket, tx, rx).await;
         });
@@ -37,7 +41,7 @@ async fn handle_client(socket: TcpStream, mut tx: Sender<String>, rx: Receiver<S
 
         // Echo back to client temporarily
         client.send(query.clone()).await?;
-        let _ = tx.send(query).await;
+        let _ = tx.send(query);
     }
 }
 
@@ -45,8 +49,8 @@ pub async fn respond(mut rx: Receiver<String>, tx: Sender<String>) -> Result<(),
     loop {
         let received = rx.recv().await;
         let received = match received {
-            Some(x) => x,
-            None => panic!("received nothing and exploded."),
+            Ok(x) => x,
+            Err(e) => panic!("received nothing and exploded."),
         };
         println!("Receiver got: {}", received);
     }
