cf42ae99360cd349332209700c1eb2a7d90774e2
Matthew Bunt <darakus@dagron.net>
2020-10-01 07:14:08 -0700

    Messy but working channels (kinda)

--- src/server.rs
+++ src/server.rs
@@ -1,19 +1,25 @@
-use futures::{StreamExt, SinkExt};
+use futures::prelude::*;
 use std::error::Error;
+use tokio::prelude::*;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::{broadcast};
 use tokio::sync::broadcast::{Sender, Receiver};
 use tokio_util::codec::{Framed, LinesCodec};
 
 pub async fn serve() -> Result<(), Box<dyn Error>> {
+    // Setup socket
     let binding = ":::10113";
     let mut listener = TcpListener::bind(&binding).await?;
+
+    // bidirectional communication channels
     let (client_tx, respond_rx) = broadcast::channel(100);
     let (respond_tx, client_rx) = broadcast::channel(100);
     
+    // created to satisfy the borrow checker
     let r_tx = respond_tx.clone();
     let r_rx = client_tx.subscribe();
 
+    // start responder loop
     tokio::spawn(async move {
         let _ = respond(r_rx, r_tx).await;
     });
@@ -28,24 +34,27 @@ pub async fn serve() -> Result<(), Box<dyn Error>> {
     }
 }
 
-async fn handle_client(socket: TcpStream, mut tx: Sender<String>, rx: Receiver<String>) -> Result<(), Box<dyn Error>> {
+async fn handle_client(socket: TcpStream, mut tx: Sender<String>, mut rx: Receiver<String>) -> Result<(), Box<dyn Error>> {
     let remote_ip = socket.peer_addr()?.ip();
     println!("Incoming connection from {}", remote_ip);
 
     let mut client = Framed::new(socket, LinesCodec::new_with_max_length(1024));
     loop {
-        let query = match client.next().await {
-            Some(Ok(q)) => q,
-            _ => return Err("no query received".into()),
-        };
-
-        // Echo back to client temporarily
-        client.send(query.clone()).await?;
-        let _ = tx.send(query);
+        tokio::select! {
+            query = client.next() => {
+                println!("Hit client.next()");
+                let _ = tx.send(query.unwrap().unwrap().clone());
+            }
+            query = rx.recv() => {
+                // TODO
+                println!("Hit rx.recv");
+                let _ = client.send(query.unwrap().clone()).await?;
+            }
+        }
     }
 }
 
-pub async fn respond(mut rx: Receiver<String>, tx: Sender<String>) -> Result<(), Box<dyn Error>> {
+async fn respond(mut rx: Receiver<String>, tx: Sender<String>) -> Result<(), Box<dyn Error>> {
     loop {
         let received = rx.recv().await;
         let received = match received {
@@ -53,5 +62,6 @@ pub async fn respond(mut rx: Receiver<String>, tx: Sender<String>) -> Result<(),
             Err(e) => panic!("received nothing and exploded."),
         };
         println!("Receiver got: {}", received);
+        tx.send(received).unwrap();
     }
 }
